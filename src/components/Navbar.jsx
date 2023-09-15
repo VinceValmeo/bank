dbb714e6fd44ce0c06f11e90b850e69dadeba9c9
Vince Valmeo <vince.valmeo.dev@gmail.com>
2023-09-15 10:49:30 +0800

    Responsive Navbar

--- src/components/Navbar.jsx
+++ src/components/Navbar.jsx
@@ -1,7 +1,60 @@
-import React from "react";
-
+import React, { useState } from "react";
+import { close, logo, menu } from "../assets";
+import { navLinks } from "../constants";
 const Navbar = () => {
-  return <div>Navbar</div>;
+  const [toggle, settoggle] = useState(false);
+  return (
+    <nav className="w-full flex py-6 justify-between items-center navbar">
+      <img src={logo} alt="bank" className="w-[124px] h-[32px]" />
+      <ul className="list-none sm:flex hidden justify-end items-center flex-1">
+        {navLinks.map((nav, index) => (
+          <li
+            key={nav.id}
+            className={`font-poppins font-normal cursor-pointer text-[16px] text-white ${
+              index === navLinks.length - 1 ? "mr-0" : "mr-10"
+            }`}
+          >
+            <a href={`#${nav.id}`} className="text-white flex justify-between">
+              {nav.title}
+            </a>
+          </li>
+        ))}
+      </ul>
+
+      <div className="sm:hidden flex flex-1 justify-end items-center ">
+        <img
+          src={toggle ? close : menu}
+          alt="menu"
+          className="w-[28px] h-[28] object-contain cursor-pointer"
+          onClick={() => settoggle((prev) => !prev)}
+        />
+
+        <div
+          className={`${
+            toggle ? `flex` : `hidden`
+          } p-6 bg-black-gradient absolute top-20 right-0 mx-4 my-2 min-w-[140px] rounded-xl sidebar`}
+        >
+          <ul className="list-none flex flex-col items-end flex-1">
+            {navLinks.map((nav, index) => (
+              <li
+                key={nav.id}
+                className={`font-poppins font-normal cursor-pointer text-[16px] text-white ${
+                  index === navLinks.length - 1 ? "mr-0" : "mb-4"
+                }`}
+              >
+                <a
+                  href={`#${nav.id}`}
+                  className="text-white flex justify-between"
+                >
+                  {nav.title}
+                </a>
+              </li>
+            ))}
+          </ul>
+        </div>
+      </div>
+    </nav>
+  );
 };
 
 export default Navbar;
