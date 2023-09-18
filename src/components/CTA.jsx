6706abf63d4b9665b2c2d37631dc20d1711edae2
Vince Valmeo <vince.valmeo.dev@gmail.com>
2023-09-18 11:42:13 +0800

    Complete CTA

--- src/components/CTA.jsx
+++ src/components/CTA.jsx
@@ -1,7 +1,22 @@
-import React from "react";
+import styles from "../style";
+import Button from "./Button";
 
-const CTA = () => {
-  return <div>CTA</div>;
-};
+const CTA = () => (
+  <section className={`my-20`}>
+    <div className="bg-black-gradient-2 rounded-3xl p-20 flex items-center justify-between flex-col md:flex-row">
+      <div>
+        <h2 className={styles.heading2}>Let’s try our service now!</h2>
+
+        <p className={`${styles.paragraph} text-left max-w-[450px]`}>
+          Everything you need to accept card payments and grow your business
+          anywhere on the planet.
+        </p>
+      </div>
+      <div className="mt-10">
+        <Button />
+      </div>
+    </div>
+  </section>
+);
 
 export default CTA;
